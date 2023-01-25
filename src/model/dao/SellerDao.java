a4796c7b9b44864b4defc372d79fdad382f724ab
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-01-25 17:14:32 -0300

    DepartmentDAO and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void inset(Seller seller);
+	void update(Seller seller);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
