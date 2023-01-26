0ceef0af3c32f9b7570c01c30a6607b20d032f57
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-01-26 09:38:42 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,12 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n==== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = 1;
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
